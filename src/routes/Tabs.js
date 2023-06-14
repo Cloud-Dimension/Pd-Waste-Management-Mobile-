ae646aebe306654d39f1fc7664cbe4b19f264d12
Sainty-dev <82646950+Sainty-dev@users.noreply.github.com>
2023-06-14 18:13:36 +0200

    removed tab bar option😁

--- src/routes/Tabs.js
+++ src/routes/Tabs.js
@@ -1,53 +1,49 @@
 // App.js
 
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import AuthStack from './AuthStack';
-import HomeStack from './HomeStack';
-import ExploreStack from './ExploreStack';
-import RecycleStack from './RecycleStack';
-import ProfileStack from './ProfileStack';
-import { FontAwesome  } from "@expo/vector-icons";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import AuthStack from "./AuthStack";
+import HomeStack from "./HomeStack";
+import ExploreStack from "./ExploreStack";
+import RecycleStack from "./RecycleStack";
+import ProfileStack from "./ProfileStack";
+import { FontAwesome } from "@expo/vector-icons";
 
 const TabNavigator = createBottomTabNavigator();
 
-const Route = ({isLoggedIn}) => {
-
+const Route = ({ isLoggedIn }) => {
   return (
     <NavigationContainer>
       {!isLoggedIn ? (
         <AuthStack />
       ) : (
-        <TabNavigator.Navigator  screenOptions={({ route }) => ({
+        <TabNavigator.Navigator
+          screenOptions={({ route }) => ({
             tabBarIcon: ({ focused, size }) => {
               let iconName;
               let color = "#252525";
-    
+
               if (route.name === "HomeTab") {
                 iconName = focused ? "home" : "home";
-                color =  focused ? "#2CAC69" : "#252525";
+                color = focused ? "#2CAC69" : "#252525";
               } else if (route.name === "ExploreTab") {
                 iconName = focused ? "search" : "search";
-                color =  focused ? "#2CAC69" : "#252525";
+                color = focused ? "#2CAC69" : "#252525";
               } else if (route.name === "RecycleTab") {
                 iconName = focused ? "recycle" : "recycle";
-                color =  focused ? "#2CAC69" : "#252525";
-              }
-              else if (route.name === "ProfileTab") {
+                color = focused ? "#2CAC69" : "#252525";
+              } else if (route.name === "ProfileTab") {
                 iconName = focused ? "user" : "user";
-                color =  focused ? "#2CAC69" : "#252525";
+                color = focused ? "#2CAC69" : "#252525";
               }
 
-              return <FontAwesome  name={iconName} size={size} color={color} />;
+              return <FontAwesome name={iconName} size={size} color={color} />;
             },
             headerShown: false,
-            tabBarLabelStyle:{display:'none'}
+            tabBarLabelStyle: { display: "none" },
           })}
-          tabBarOptions={{
-            activeTintColor: "gray",
-            inactiveTintColor: "gray",
-          }}>
+        >
           <TabNavigator.Screen name="HomeTab" component={HomeStack} />
           <TabNavigator.Screen name="ExploreTab" component={ExploreStack} />
           <TabNavigator.Screen name="RecycleTab" component={RecycleStack} />
